5fe4d79200ff5b9929200d275f3e4b2c04a439d5
arnite <udembao429@gmail.com>
2025-01-11 04:28:26 +1400

    Added eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': ['error', { trailingComma: 'es5' }],
+      'comma-dangle': ['off'],
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020, // or a version you prefer
+      },
+    },
+  },
+];
